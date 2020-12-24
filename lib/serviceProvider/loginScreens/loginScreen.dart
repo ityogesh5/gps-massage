5e51050608a1da29b8b60e2d7524c03848a20b88
Saravanakumar <Saravanakumar.b@nexware-global.com>
2020-12-24 13:29:25 +0530

    validation added for provider login & forget password

--- lib/serviceProvider/loginScreens/loginScreen.dart
+++ lib/serviceProvider/loginScreens/loginScreen.dart
@@ -9,6 +9,9 @@ import 'package:gps_massageapp/serviceProvider/registerProvider/registerFirstScr
 import 'package:gps_massageapp/serviceProvider/MyHomePage.dart';
 import 'package:gps_massageapp/serviceProvider/registerProvider/providerHome.dart';
 import 'package:gps_massageapp/serviceProvider/registerProvider/registerSecondScreen.dart';
+import 'package:gps_massageapp/constantUtils/colorConstants.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   @override
@@ -17,10 +20,27 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool passwordVisibility = true;
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final phoneNumberController = new TextEditingController();
+  final passwordController = new TextEditingController();
+
+  FocusNode phoneNumberFocus = FocusNode();
+  FocusNode createPasswordFocus = FocusNode();
+
+  List<String> serviceProviderLoginDetails = [];
+
+  //Regex validation for emojis in text
+  RegExp regexEmojis = RegExp(
+      r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])');
+
+  //..updated regex pattern
+  RegExp passwordRegex = new RegExp(
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~!?@#$%^&*_-]).{8,}$');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: SafeArea(
@@ -59,6 +79,10 @@ class _LoginState extends State<Login> {
                       height: 20,
                     ),
                     TextFormField(
+                      textInputAction: TextInputAction.done,
+                      focusNode: phoneNumberFocus,
+                      controller: phoneNumberController,
+                      keyboardType: TextInputType.phone,
                       decoration: new InputDecoration(
                           enabledBorder: OutlineInputBorder(
                             borderRadius: const BorderRadius.all(
@@ -79,6 +103,10 @@ class _LoginState extends State<Login> {
                       height: 20,
                     ),
                     TextFormField(
+                      textInputAction: TextInputAction.done,
+                      focusNode: createPasswordFocus,
+                      controller: passwordController,
+                      obscureText: passwordVisibility,
                       decoration: new InputDecoration(
                           enabledBorder: OutlineInputBorder(
                             borderRadius: const BorderRadius.all(
@@ -112,11 +140,7 @@ class _LoginState extends State<Login> {
                       children: [
                         InkWell(
                           onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        ForgetPassword()));
+                            NavigationRouter.switchToProviderForgetPasswordScreen(context);
                           },
                           child: Text(
                             HealingMatchConstants.loginForgetPassword,
@@ -140,7 +164,8 @@ class _LoginState extends State<Login> {
                         ),
                         color: Colors.lime,
                         onPressed: () {
-                          NavigationRouter.switchToProviderHome(context);
+                          _providerLoginDetails();
+                          //NavigationRouter.switchToProviderHome(context);
                         },
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10.0),
@@ -258,4 +283,104 @@ class _LoginState extends State<Login> {
       ),
     );
   }
+
+  _providerLoginDetails() async {
+    var userPhoneNumber = phoneNumberController.text.toString();
+    var password = passwordController.text.toString();
+
+    // user phone number validation
+    if (userPhoneNumber.length > 11 ||
+        userPhoneNumber == null ||
+        userPhoneNumber.isEmpty) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: ColorConstants.snackBarColor,
+        content: Text('11文字以上の電話番号を入力してください。',
+            style: TextStyle(fontFamily: 'Open Sans')),
+        action: SnackBarAction(
+            onPressed: () {
+              _scaffoldKey.currentState.hideCurrentSnackBar();
+            },
+            label: 'はい',
+            textColor: Colors.white),
+      ));
+      return;
+    }
+
+    if (password.length < 8) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: ColorConstants.snackBarColor,
+        content: Text('パスワードは8文字以上で入力してください。  ',
+            style: TextStyle(fontFamily: 'Open Sans')),
+        action: SnackBarAction(
+            onPressed: () {
+              _scaffoldKey.currentState.hideCurrentSnackBar();
+            },
+            label: 'はい',
+            textColor: Colors.white),
+      ));
+      return;
+    }
+
+    if (password.length > 14) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: ColorConstants.snackBarColor,
+        content: Text('パスワードは15文字以内で入力してください。 ',
+            style: TextStyle(fontFamily: 'Open Sans')),
+        action: SnackBarAction(
+            onPressed: () {
+              _scaffoldKey.currentState.hideCurrentSnackBar();
+            },
+            label: 'はい',
+            textColor: Colors.white),
+      ));
+      return;
+    }
+
+    // Combination password
+
+    if (!passwordRegex.hasMatch(password)) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: ColorConstants.snackBarColor,
+        content: Text('パスワードには、大文字、小文字、数字、特殊文字を1つ含める必要があります。'),
+        action: SnackBarAction(
+            onPressed: () {
+              _scaffoldKey.currentState.hideCurrentSnackBar();
+            },
+            label: 'はい',
+            textColor: Colors.white),
+      ));
+      return;
+    }
+
+    if (password.contains(regexEmojis)) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: ColorConstants.snackBarColor,
+        content: Text('有効な文字でパスワードを入力してください。',
+            style: TextStyle(fontFamily: 'Open Sans')),
+        action: SnackBarAction(
+            onPressed: () {
+              _scaffoldKey.currentState.hideCurrentSnackBar();
+            },
+            label: 'はい',
+            textColor: Colors.white),
+      ));
+      return;
+    }
+
+    serviceProviderLoginDetails.add(userPhoneNumber);
+    serviceProviderLoginDetails.add(password);
+
+    print('User details length in array : ${serviceProviderLoginDetails.length}');
+
+    final url = '';
+    http.post(url,
+        headers: {
+          "Accept": "application/json",
+          "Authorization": "Bearer ${'token'}"
+        },
+        body: json.encode({
+          "serviceUserDetails": serviceProviderLoginDetails,
+        }));
+  }
+
 }
