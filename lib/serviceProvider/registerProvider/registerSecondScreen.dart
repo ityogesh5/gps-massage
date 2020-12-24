425f8e922d882b019d8a0f348d48bca6f5f17a20
Gowtham <gowtham.n@nexware-global.com>
2020-12-24 14:37:22 +0530

    changes in Registration ServiceProvider

--- lib/serviceProvider/registerProvider/registerSecondScreen.dart
+++ lib/serviceProvider/registerProvider/registerSecondScreen.dart
@@ -96,6 +96,7 @@ class _RegistrationSecondPageState extends State<RegistrationSecondPage> {
                               setState(() {
                                 identificationverify = value;
                               });
+
                             },
                             dataSource: [
                               {
