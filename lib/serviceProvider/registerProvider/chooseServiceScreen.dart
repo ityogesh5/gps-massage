ae4000854e9f61fb074ea9c44bcad0370e42df7b
DESKTOP-89O7H84\NEX1035 <aswin.a@nexware-global.com>
2020-12-30 13:58:14 +0530

    Fixed ChooseServiceScreen Bugs, Added Proper Comments

--- lib/serviceProvider/registerProvider/chooseServiceScreen.dart
+++ lib/serviceProvider/registerProvider/chooseServiceScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math' as math;
+import 'package:gps_massageapp/constantUtils/colorConstants.dart';
 import 'package:gps_massageapp/customLibraryClasses/progressDialogs/custom_dialog.dart';
 import 'package:gps_massageapp/models/apiResponseModels/estheticDropDownModel.dart';
 import 'package:gps_massageapp/models/apiResponseModels/relaxationDropDownModel.dart';
@@ -70,7 +71,6 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
   List<String> treatmentDropDownValues = List<String>();
   List<String> relaxationDropDownValues = List<String>();
   List<String> fitnessDropDownValues = List<String>();
-  List<String> dropDownMenuItems = List<String>();
   List<ServicePriceModel> estheticServicePriceModel = List<ServicePriceModel>();
   List<ServicePriceModel> relaxationServicePriceModel =
       List<ServicePriceModel>();
@@ -87,22 +87,15 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
   int treatmentStatus = 0;
   int fitnessStatus = 0;
   TextEditingController sampleOthersController = TextEditingController();
-  List<String> dropDownValues = List<String>();
   EstheticDropDownModel estheticListModel;
   RelaxationDropDownModel relaxationDropDownModel;
   TreatmentDropDownModel treatmentDropDownModel;
   ProgressDialog _progressDialog = ProgressDialog();
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
-    dropDownValues = [
-      'はり（足）',
-      '美容鍼（顔）',
-      'きゅう（足）',
-      'マッサージ（全身）',
-      HealingMatchConstants.chooseServiceOtherDropdownFiled
-    ];
     fitnessDropDownValues = [
       "ヨガ",
       "ホットヨガ",
@@ -110,12 +103,6 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
       "トレーニング,"
           "エクササイズ",
       HealingMatchConstants.chooseServiceOtherDropdownFiled
-    ];
-    dropDownMenuItems = [
-      HealingMatchConstants.chooseServiceEstheticDropDown,
-      HealingMatchConstants.chooseServiceRelaxationDropDown,
-      HealingMatchConstants.chooseServiceTreatmentDropDown,
-      HealingMatchConstants.chooseServiceFitnessDropDown
     ];
     otherSelected = [false, false, false, false];
   }
@@ -132,6 +119,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: SafeArea(
         child: SingleChildScrollView(
           primary: true,
@@ -513,6 +501,8 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
     );
   }
 
+  //Build CheckBox Contents for the Dropdown Function for the list got from APi
+  //mindex represents the dropdown values 0-Esthetic 1-Relaxation 2-Treatment 3-Fitness
   buildCheckBoxContent(String val, int index, int mindex) {
     bool checkValue = false;
     int indexPos = 0;
@@ -556,10 +546,12 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                     onChanged: (bool newValue) {
                       setState(() {
                         if (newValue == null) {
+                          //if the value gets unselected in checkbox
                           checkValue = false;
                           if (val !=
                               HealingMatchConstants
                                   .chooseServiceOtherDropdownFiled) {
+                            //for values other than the field "other"
                             selectedDropdownValues.remove(val.toLowerCase());
                             servicePriceModel.removeAt(indexPos);
                             if (mindex == 0) {
@@ -595,12 +587,15 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                             otherSelected[mindex] = false;
                           }
                         } else {
+                          //if the value is selected
                           if (val ==
                               HealingMatchConstants
                                   .chooseServiceOtherDropdownFiled) {
+                            // for the field other
                             checkValue = true;
                             otherSelected[mindex] = true;
                           } else {
+                            //for the remaining fields
                             showTimeandPriceDialog(
                                 val, checkValue, indexPos, mindex);
                           }
@@ -612,6 +607,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                 ],
               ),
             ),
+            //for the Edit option displayed
             val != HealingMatchConstants.chooseServiceOtherDropdownFiled &&
                     checkValue == true
                 ? Expanded(
@@ -635,6 +631,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                 : Container()
           ],
         ),
+        //other addition field display
         (checkValue == true &&
                 val == HealingMatchConstants.chooseServiceOtherDropdownFiled)
             ? Column(
@@ -668,19 +665,39 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                                 FocusScope.of(context)
                                     .requestFocus(new FocusNode());
                                 setState(() {
-                                  estheticDropDownValues = List<String>();
+                                  //Logic: check the value is already present, if not insert before other field index
                                   if (mindex == 0) {
-                                    estheticDropDownValues.insert(
-                                        index, sampleOthersController.text);
+                                    if (estheticDropDownValues.contains(
+                                        sampleOthersController.text)) {
+                                      showErrorMessage();
+                                    } else {
+                                      estheticDropDownValues.insert(
+                                          index, sampleOthersController.text);
+                                    }
                                   } else if (mindex == 1) {
-                                    relaxationDropDownValues.insert(
-                                        index, sampleOthersController.text);
+                                    if (relaxationDropDownValues.contains(
+                                        sampleOthersController.text)) {
+                                      showErrorMessage();
+                                    } else {
+                                      relaxationDropDownValues.insert(
+                                          index, sampleOthersController.text);
+                                    }
                                   } else if (mindex == 2) {
-                                    treatmentDropDownValues.insert(
-                                        index, sampleOthersController.text);
+                                    if (treatmentDropDownValues.contains(
+                                        sampleOthersController.text)) {
+                                      showErrorMessage();
+                                    } else {
+                                      treatmentDropDownValues.insert(
+                                          index, sampleOthersController.text);
+                                    }
                                   } else if (mindex == 3) {
-                                    fitnessDropDownValues.insert(
-                                        index, sampleOthersController.text);
+                                    if (fitnessDropDownValues.contains(
+                                        sampleOthersController.text)) {
+                                      showErrorMessage();
+                                    } else {
+                                      fitnessDropDownValues.insert(
+                                          index, sampleOthersController.text);
+                                    }
                                   }
                                   sampleOthersController.clear();
                                 });
@@ -710,6 +727,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                 ],
               )
             : Container(),
+        //Build the MultiSelect Chips below the selected CheckBox
         (checkValue == true &&
                 val != HealingMatchConstants.chooseServiceOtherDropdownFiled)
             ? Padding(
@@ -727,6 +745,8 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
     );
   }
 
+  //Build the chips for the price greater than 0
+  //indexpos represents the postion of the List Item in the pricemodel
   checkAddedPrice(int indexPos, List<ServicePriceModel> servicePriceModel) {
     List<Widget> values = List<Widget>();
     if ((servicePriceModel[indexPos].sixtyMin) != 0) {
@@ -747,24 +767,16 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
       values.add(
           buildCheckBoxChip(servicePriceModel[indexPos].oneEightyMin, 180));
     }
-    //For Alignment Purposes
-    /*   if (values.length < 3) {
-      if (values.length == 1) {
-        values.add(Container());
-      } else {
-        values.add(Container());
-        values.add(Container());
-      }
-    } */
     return values;
   }
 
+  //Design for the chips built which price greater than 0
+  //min - minutes for which price is set.
   buildCheckBoxChip(int price, int min) {
     return Container(
       padding: EdgeInsets.all(10.0),
       height: 40.0,
       width: 110.0,
-      //padding: const EdgeInsets.only(left: 10.0, right: 10.0),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
           border: Border.all(
@@ -779,6 +791,9 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
     );
   }
 
+  //Popup Screen for setting the price
+  //used mindex for checking which List should the selected value be stored
+  //0-Esthetic 1-Relaxation 2-Treatment 3-Fitness
   showTimeandPriceDialog(String val, bool selected, int indexPos, int mindex) {
     TextEditingController sixtyMinutesController = new TextEditingController();
     TextEditingController nintyMinuteController = new TextEditingController();
@@ -824,8 +839,6 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
           elevation: 16,
           child: SingleChildScrollView(
             child: Container(
-              //height: 300.0,
-              //width: 450.0,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
@@ -1161,7 +1174,10 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                                           borderRadius:
                                               new BorderRadius.circular(10.0)),
                                       onPressed: () {
+                                        print(estheticServicePriceModel);
+                                        //if checkbox slected for the first time
                                         if (!selected) {
+                                          //check if any values or entered or all empty
                                           if (sixtyMinutesController.text !=
                                                   "" ||
                                               nintyMinuteController.text !=
@@ -1171,6 +1187,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                                               oneFiftyController.text != "" ||
                                               oneEightyMinuteController.text !=
                                                   "") {
+                                            //if entered then the empty values are treated as 0
                                             servicePriceModel.add(
                                               ServicePriceModel(
                                                 val,
@@ -1208,6 +1225,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                                             selectedDropdownValues
                                                 .add(val.toLowerCase());
                                             setState(() {
+                                              //checking with the mindex the values are added to corresponding lists
                                               if (mindex == 0) {
                                                 selectedEstheticDropdownValues
                                                     .clear();
@@ -1251,7 +1269,9 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
                                               }
                                             });
                                           }
-                                        } else {
+                                        }
+                                        //if checkbox already selected then update logic is used
+                                        else {
                                           servicePriceModel[indexPos].sixtyMin =
                                               sixtyMinutesController.text != ""
                                                   ? int.parse(
@@ -1330,6 +1350,7 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
     );
   }
 
+  //get the index of the String on the List
   int getIndex(String val, List<ServicePriceModel> servicePriceModel) {
     int indexPos;
     for (int i = 0; i < servicePriceModel.length; i++) {
@@ -1393,5 +1414,21 @@ class _ChooseServiceScreenState extends State<ChooseServiceScreen> {
         treatmentStatus == 0 ? treatmentStatus = 1 : treatmentStatus = 0;
       });
     });
+  }
+
+  //show the SnackBar Error
+  void showErrorMessage() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      backgroundColor: ColorConstants.snackBarColor,
+      content: Text('Value already exists',
+          style: TextStyle(fontFamily: 'Open Sans')),
+      action: SnackBarAction(
+          onPressed: () {
+            _scaffoldKey.currentState.hideCurrentSnackBar();
+          },
+          label: 'はい',
+          textColor: Colors.white),
+    ));
+    return;
   }
 }
