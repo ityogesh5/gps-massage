e94e4575ff976af46ea346b050ea945b4be834f9
Gowtham <gowtham.n@nexware-global.com>
2021-01-06 19:10:43 +0530

    TextChangesServiceProvider

--- lib/serviceProvider/registerProvider/registerFirstScreen.dart
+++ lib/serviceProvider/registerProvider/registerFirstScreen.dart
@@ -1282,10 +1282,9 @@ class _RegisterFirstScreenState extends State<RegisterProviderFirstScreen> {
                       borderRadius: new BorderRadius.circular(10.0)),
                   onPressed: () {
                     //!Commented for Dev purposes
-                    //validateFields();
+                    validateFields();
 
-                    NavigationRouter.switchToServiceProviderSecondScreen(
-                        context);
+                    // NavigationRouter.switchToServiceProviderSecondScreen(context);
                   },
                 ),
               ),
