ef91adccea26c5b61bdb4c51d44bb252a50108b5
Yogeshwar <yogeshwar.p@nexware-global.com>
2021-03-02 17:16:48 +0530

    Pubspec libraries definition edit..Commented

--- lib/serviceProvider/homeScreens/myAccount/ProviderEditProfile.dart
+++ lib/serviceProvider/homeScreens/myAccount/ProviderEditProfile.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -10,11 +9,7 @@ import 'package:gps_massageapp/constantUtils/constantsUtils.dart';
 import 'package:gps_massageapp/constantUtils/helperClasses/progressDialogsHelper.dart';
 import 'package:gps_massageapp/constantUtils/helperClasses/statusCodeResponseHelper.dart';
 import 'package:gps_massageapp/customLibraryClasses/dropdowns/dropDownServiceUserRegisterScreen.dart';
-import 'package:gps_massageapp/customLibraryClasses/progressDialogs/custom_dialog.dart';
-import 'package:gps_massageapp/models/responseModels/serviceProvider/bankNameDropDownModel.dart';
 import 'package:gps_massageapp/models/responseModels/serviceProvider/cityList.dart';
-import 'package:gps_massageapp/models/responseModels/serviceProvider/loginResponseModel.dart'
-    as loginResponse;
 import 'package:gps_massageapp/models/responseModels/serviceProvider/providerProfileUpdateResponseModel.dart'
     as profileUpdate;
 import 'package:gps_massageapp/models/responseModels/serviceProvider/stateList.dart';
@@ -23,6 +18,12 @@ import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:gps_massageapp/customLibraryClasses/progressDialogs/custom_dialog.dart';
+import 'package:flutter_absolute_path/flutter_absolute_path.dart';
+import 'package:gps_massageapp/serviceProvider/homeScreens/myAccount/MyAccount.dart';
+import 'package:gps_massageapp/models/responseModels/serviceProvider/bankNameDropDownModel.dart';
+import 'package:gps_massageapp/models/responseModels/serviceProvider/loginResponseModel.dart'
+    as loginResponse;
 import 'package:shared_preferences/shared_preferences.dart';
 
 List<File> files = List<File>();
--- lib/serviceUser/homeScreen/bookingScreensUser/BookingDetailScreens/ApprovalWaitingScreen.dart
+++ lib/serviceUser/homeScreen/bookingScreensUser/BookingDetailScreens/ApprovalWaitingScreen.dart
@@ -4,9 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gps_massageapp/constantUtils/constantsUtils.dart';
-import 'package:gps_massageapp/routing/navigationRouter.dart';
-
 import 'file:///C:/Users/user1/Documents/HealingMatch%20App/gps-massage/lib/customFavoriteButton/CustomHeartFavorite.dart';
+import 'package:gps_massageapp/routing/navigationRouter.dart';
 
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
