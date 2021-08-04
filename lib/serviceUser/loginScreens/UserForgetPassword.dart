7a88628c1a02b8c0dedbb7387e1bc5c3cd52ab67
Aswin <>
2021-08-04 17:56:13 +0530

    forgot password ios done button fix

--- lib/serviceUser/loginScreens/UserForgetPassword.dart
+++ lib/serviceUser/loginScreens/UserForgetPassword.dart
@@ -5,10 +5,12 @@ import 'package:gps_massageapp/constantUtils/colorConstants.dart';
 import 'package:gps_massageapp/constantUtils/constantsUtils.dart';
 import 'package:gps_massageapp/constantUtils/helperClasses/progressDialogsHelper.dart';
 import 'package:gps_massageapp/constantUtils/helperClasses/statusCodeResponseHelper.dart';
+import 'package:gps_massageapp/customLibraryClasses/keyboardDoneButton/keyboardActionConfig.dart';
 import 'package:gps_massageapp/models/responseModels/serviceUser/login/sendVerifyResponseModel.dart';
 import 'package:gps_massageapp/routing/navigationRouter.dart';
 import 'package:gps_massageapp/serviceUser/loginScreens/UserLoginScreen.dart';
 import 'package:http/http.dart' as http;
+import 'package:keyboard_actions/keyboard_actions.dart';
 
 class UserForgetPassword extends StatefulWidget {
   @override
@@ -45,100 +47,103 @@ class _UserForgetPasswordState extends State<UserForgetPassword> {
           },
         ),
       ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).requestFocus(new FocusNode());
-        },
-        child: Form(
-          key: formKey,
-          autovalidate: autoValidate,
-          child: Center(
-            child: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(left: 20, right: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FittedBox(
-                      child: Text(
-                        HealingMatchConstants.userPasswordTxt1,
-                        style: TextStyle(
-                            fontFamily: 'NotoSansJP',
-                            color: Color.fromRGBO(102, 102, 102, 1)),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        HealingMatchConstants.userPasswordTxt2,
-                        style: TextStyle(
-                            fontFamily: 'NotoSansJP',
-                            color: Color.fromRGBO(102, 102, 102, 1)),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        HealingMatchConstants.userPasswordTxt3,
-                        style: TextStyle(
-                            fontFamily: 'NotoSansJP',
-                            color: Color.fromRGBO(102, 102, 102, 1)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    TextFormField(
-                      maxLength: 11,
-                      textInputAction: TextInputAction.done,
-                      style: HealingMatchConstants.formTextStyle,
-                      focusNode: phoneNumberFocus,
-                      controller: phoneNumberController,
-                      keyboardType: TextInputType.phone,
-                      decoration: new InputDecoration(
-                        counterText: '',
-                        contentPadding: EdgeInsets.fromLTRB(6, 3, 6, 3),
-                        border: HealingMatchConstants.textFormInputBorder,
-                        focusedBorder:
-                            HealingMatchConstants.textFormInputBorder,
-                        disabledBorder:
-                            HealingMatchConstants.textFormInputBorder,
-                        enabledBorder:
-                            HealingMatchConstants.textFormInputBorder,
-                        filled: true,
-                        labelText: HealingMatchConstants.userPasswordPhn,
-                        labelStyle: HealingMatchConstants.formLabelTextStyle,
-                        /* hintStyle: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'NotoSansJP',
-                            fontSize: 13),
-                        hintText: HealingMatchConstants.forgetPasswordPhn,*/
-                        fillColor: ColorConstants.formFieldFillColor,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 21,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 41,
-                      child: RaisedButton(
+      body: KeyboardActions(
+        config: KeyboardCustomActions().buildConfig(context, phoneNumberFocus),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Form(
+            key: formKey,
+            autovalidate: autoValidate,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FittedBox(
                         child: Text(
-                          HealingMatchConstants.userForgetPassBtn,
+                          HealingMatchConstants.userPasswordTxt1,
                           style: TextStyle(
-                              color: Colors.white,
                               fontFamily: 'NotoSansJP',
-                              fontSize: 20),
-                        ),
-                        color: Color.fromRGBO(200, 217, 33, 1),
-                        onPressed: () {
-                          _userForgetPasswordDetails();
-                        },
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                              color: Color.fromRGBO(102, 102, 102, 1)),
                         ),
                       ),
-                    ),
-                  ],
+                      FittedBox(
+                        child: Text(
+                          HealingMatchConstants.userPasswordTxt2,
+                          style: TextStyle(
+                              fontFamily: 'NotoSansJP',
+                              color: Color.fromRGBO(102, 102, 102, 1)),
+                        ),
+                      ),
+                      FittedBox(
+                        child: Text(
+                          HealingMatchConstants.userPasswordTxt3,
+                          style: TextStyle(
+                              fontFamily: 'NotoSansJP',
+                              color: Color.fromRGBO(102, 102, 102, 1)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        maxLength: 11,
+                        textInputAction: TextInputAction.done,
+                        style: HealingMatchConstants.formTextStyle,
+                        focusNode: phoneNumberFocus,
+                        controller: phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        decoration: new InputDecoration(
+                          counterText: '',
+                          contentPadding: EdgeInsets.fromLTRB(6, 3, 6, 3),
+                          border: HealingMatchConstants.textFormInputBorder,
+                          focusedBorder:
+                              HealingMatchConstants.textFormInputBorder,
+                          disabledBorder:
+                              HealingMatchConstants.textFormInputBorder,
+                          enabledBorder:
+                              HealingMatchConstants.textFormInputBorder,
+                          filled: true,
+                          labelText: HealingMatchConstants.userPasswordPhn,
+                          labelStyle: HealingMatchConstants.formLabelTextStyle,
+                          /* hintStyle: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'NotoSansJP',
+                              fontSize: 13),
+                          hintText: HealingMatchConstants.forgetPasswordPhn,*/
+                          fillColor: ColorConstants.formFieldFillColor,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 21,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 41,
+                        child: RaisedButton(
+                          child: Text(
+                            HealingMatchConstants.userForgetPassBtn,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'NotoSansJP',
+                                fontSize: 20),
+                          ),
+                          color: Color.fromRGBO(200, 217, 33, 1),
+                          onPressed: () {
+                            _userForgetPasswordDetails();
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
