f44bc4bbf40901c0cfab6b49be11af449387b670
DESKTOP-9S150FA\NEX2056 <gowtham.n@nexware-global.com>
2021-08-16 19:12:08 +0530

    Testing URL change

--- lib/constantUtils/constantsUtils.dart
+++ lib/constantUtils/constantsUtils.dart
@@ -56,9 +56,9 @@ class HealingMatchConstants {
   static const String ON_PREMISE_USER_BASE_URL =
       // "http://103.92.19.158:9087/api"; //secondary backup IP
 
-      //   "http://106.51.49.160:9087/api"; // Development data URL
+        "http://106.51.49.160:9087/api"; // Development data URL
 
-      "http://106.51.49.160:9094/api"; // Testing data URL
+      // "http://106.51.49.160:9094/api"; // Testing data URL
 
   // "http://103.92.19.158:9094/api"; //Temp URL
 
