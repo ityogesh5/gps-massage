231c5be8e6017240f823cb6c04324b990af8383c
Aswin <>
2021-08-19 16:39:23 +0530

    ui bug fix user

--- lib/serviceUser/homeScreen/bookingScreensUser/BookingDetailScreens/sampleBookingScreen.dart
+++ lib/serviceUser/homeScreen/bookingScreensUser/BookingDetailScreens/sampleBookingScreen.dart
@@ -490,23 +490,32 @@ class _SampleBookingScreenState extends State<SampleBookingScreen> {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      width: 2,
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/images_gps/cost.svg",
+                      height: 14.77,
+                      width: 16.0,
                     ),
-                    Flexible(
-                      child: Text(
-                        therapistDetails.bookingDataResponse[0].travelAmount ==
-                                    0 ||
-                                therapistDetails
-                                        .bookingDataResponse[0].travelAmount ==
-                                    null
-                            ? '¥${therapistDetails.bookingDataResponse[0].priceOfService}'
-                            : '¥${therapistDetails.bookingDataResponse[0].priceOfService + therapistDetails.bookingDataResponse[0].travelAmount} (${therapistDetails.bookingDataResponse[0].addedPrice} - ¥${therapistDetails.bookingDataResponse[0].travelAmount})',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                          
-                        ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      therapistDetails.bookingDataResponse[0].travelAmount ==
+                                  0 ||
+                              therapistDetails
+                                      .bookingDataResponse[0].travelAmount ==
+                                  null
+                          ? '¥${therapistDetails.bookingDataResponse[0].priceOfService}'
+                          : '¥${therapistDetails.bookingDataResponse[0].priceOfService + therapistDetails.bookingDataResponse[0].travelAmount} (${therapistDetails.bookingDataResponse[0].addedPrice} - ¥${therapistDetails.bookingDataResponse[0].travelAmount})',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
                       ),
                     ),
                   ],
@@ -808,7 +817,7 @@ class _SampleBookingScreenState extends State<SampleBookingScreen> {
                       width: 8,
                     ),
                     Text(
-                      '¥${therapistDetails.bookingDataResponse[0].priceOfService}',
+                      '¥${therapistDetails.bookingDataResponse[0].totalCost}',
                       style: TextStyle(
                         fontSize: 14.0,
                         color: Colors.black,
